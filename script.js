c31f34dd53b81a55c290ea64cdd00578b0484e85
Amit Kumar <98144018+Amy4u@users.noreply.github.com>
2022-04-03 14:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const menubtn = document.getElementById('menu-Image');
+
+// const mobmenu = document.getElementsById('toggle-menu');
+
+// const closebtn = document.getElementById('close-img');
+
+
+document.getElementById('hambarg-menu').addEventListener('click', () => {
+    document.getElementById('toggle-menu').style.left = '0';
+    });
+
+document.getElementById('close-icon').addEventListener('click', () => {
+    document.getElementById('toggle-menu').style.left = '-100%';
+    });
